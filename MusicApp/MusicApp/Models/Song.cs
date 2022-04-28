35cad73a3f97ac4d71619f5c1e065d548570d88b
Pavlo Tymofeienko <39266935+inovarka@users.noreply.github.com>
2022-04-28 11:55:31 +0300

    Add Song Model

---
+++ MusicApp/MusicApp/Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApp.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string Address { get; set; }
+    }
+}
