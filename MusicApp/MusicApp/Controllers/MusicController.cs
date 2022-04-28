53167572f8b65a7154cdf5347d00623138779346
Pavlo Tymofeienko <39266935+inovarka@users.noreply.github.com>
2022-04-28 14:44:10 +0300

    Add Music Controller

---
+++ MusicApp/MusicApp/Controllers/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Services;
+
+namespace MusicApp.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("[controller]")]
+    public class MusicController : Controller
+    {
+        private readonly SongService songService;
+
+        public MusicController(SongService songService)
+        {
+            this.songService = songService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Songs = songService.GetList();
+            return View();
+        }
+
+
+    }
+}
